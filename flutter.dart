a24a376b20a1d4e0f22d49e8dfd727e6e1072446
zeroxmambetov-a11y <zeroxmambetov@gmail.com>
2026-09-18 19:41:00 +0600

    first commit

---
+++ flutter.dart
@@ -0,0 +1,20 @@
+// constants.dart
+import 'package:flutter/material.dart';
+
+const double cardRadius = 16;
+const double imageHeight = 120;
+const double paddingSize = 12;
+const Color priceColor = Colors.green;
+
+const Color categoryTagColor = Colors.blue;
+
+const List<BoxShadow> cardShadow = [
+  BoxShadow(
+    color: Color(0x14000000),
+    blurRadius: 12,
+    offset: Offset(0, 4),
+    spreadRadius: 1,
+  ),
+];
+
+const Color cardBackgroundColor = Color(0xFFFFFDF5);
